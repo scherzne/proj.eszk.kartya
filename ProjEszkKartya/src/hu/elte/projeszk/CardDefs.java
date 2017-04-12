6a1b910624050e38ab6eb6cbc1ce3415e23e9a96
betti <betti@betti-laptop-PC>
2017-04-12 20:08:04 +0200

    Fekete színű típussal kiegészítve, mert lemaradt

--- ProjEszkKartya/src/hu/elte/projeszk/CardDefs.java
+++ ProjEszkKartya/src/hu/elte/projeszk/CardDefs.java
@@ -1,15 +1,21 @@
 package hu.elte.projeszk;
 
+/**
+ * Konstansok a kártyákhoz
+ * @author betti-laptop
+ *
+ */
 public class CardDefs {
 	public static final int colorGreen=0;
 	public static final int colorBlue=1;
 	public static final int colorRed=2;
 	public static final int colorYellow=3;
+	public static final int colorBlack=9;
 	
 	public static final int turnBack=4;
 	public static final int handTwo=5;
 	public static final int handFour=6;
 	public static final int jumpCard=7;
 	public static final int jokerCard=8;
-	
+		
 }

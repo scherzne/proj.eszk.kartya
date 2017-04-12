97019c78a61b770f936cc9f1354a4cb88ea53ae5
betti <betti@betti-laptop-PC>
2017-04-12 19:47:04 +0200

    Ez egy próba, megpróbáltam az Eclipse-t beállítani, és létrehoztam egy class-t, amiben a kártya definíciók lesznek.

---
+++ ProjEszkKartya/src/hu/elte/projeszk/CardDefs.java
@@ -0,0 +1,15 @@
+package hu.elte.projeszk;
+
+public class CardDefs {
+	public static final int colorGreen=0;
+	public static final int colorBlue=1;
+	public static final int colorRed=2;
+	public static final int colorYellow=3;
+	
+	public static final int turnBack=4;
+	public static final int handTwo=5;
+	public static final int handFour=6;
+	public static final int jumpCard=7;
+	public static final int jokerCard=8;
+	
+}

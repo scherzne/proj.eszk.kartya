5506c95e334cb44639901d3a812afd940dccc444
Gyula <Gyula@LAPTOP-6DU3L4Q0>
2017-04-15 18:12:57 +0200

    Kártya osztályhoz alapvető teszt osztály

---
+++ ProjEszkKartya/src/hu/elte/projeszk/TestCardClass.java
@@ -0,0 +1,31 @@
+package hu.elte.projeszk;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import  hu.elte.projeszk.Card;
+import  hu.elte.projeszk.Card.CardColor;
+import hu.elte.projeszk.Card.CardValue;
+
+
+public class TestCardClass {
+	
+	 final CardColor cardColor = CardColor.F;
+	 final CardValue cardValue = CardValue.HAROM;
+	 
+	  @Test
+	    public void CardContructorTest() {
+		  
+		   
+		  Card myCard = new Card(cardColor,cardValue);
+
+	       // String result = myUnit.concatenate("one", "two");
+	       // assertEquals("onetwo", result);
+		  
+		  assertEquals(CardColor.F ,myCard.getCardColor());
+		  assertEquals(CardValue.HAROM ,myCard.getCardValue());
+		  
+		//  assertEquals(CardValue.NEGY ,myCard.getCardValue());
+	  }
+}

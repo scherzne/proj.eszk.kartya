910a18a028ee36f61d21a95e88cd4f991b5c352b
Gyula <Gyula@LAPTOP-6DU3L4Q0>
2017-04-15 18:12:28 +0200

    Tovább írt kártya osztály enumokkal megvalósítva

--- ProjEszkKartya/src/hu/elte/projeszk/Card.java
+++ ProjEszkKartya/src/hu/elte/projeszk/Card.java
@@ -1,5 +1,8 @@
 package hu.elte.projeszk;
 
+import  hu.elte.projeszk.Card.CardColor;
+import  hu.elte.projeszk.Card.CardValue;
+
 /**
  * Kártya objektum, adat küldés egyszerűsítése miatt csak int-ek határozzák meg
  * a fajtáját.
@@ -7,17 +10,33 @@ package hu.elte.projeszk;
  *
  */
 public class Card {
-	private int color;
+	public enum CardColor {S, K, Z, P,F;} ;
+	public enum CardValue {NULLA,EGY, KETTO, HAROM, NEGY, OT, HAT, HET, NYOLC,KILENC,FORDITTO,HUZZKETTOT,KIMARADSZ,SZINKEREO,HUZZNEGYET} ;
+	
+	CardValue cardValue;
+	CardColor cardColor;
 	private int value;
 	
-	public Card(int color, int value){
-		this.color=color;
-		this.value=value;
+	
+	public Card(CardColor cardcolor, CardValue cardValue){
+		this.cardValue=cardValue;
+		this.cardColor=cardcolor;
+		
+	//	switch (cardValue){
+	//	case (0): break;
+		
+	//	}		
+
 	}
 
-	public int getColor() {
-		return color;
+	public CardColor getCardColor() {
+		return cardColor;
 	}
+	
+	public CardValue getCardValue() {
+		return cardValue;
+	}
+
 
 	public int getValue() {
 		return value;

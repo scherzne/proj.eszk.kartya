7f265bc34ba1d0ddde0bbc4aa5085d7da618a02d
Gyula <Gyula@LAPTOP-6DU3L4Q0>
2017-04-15 19:57:04 +0200

    Gépi Kliens osztály váz 3 függvényel

---
+++ ProjEszkKartya/src/hu/elte/projeszk/ClientMachine.java
@@ -0,0 +1,48 @@
+package hu.elte.projeszk;
+
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import hu.elte.projeszk.Card;
+import hu.elte.projeszk.Card.CardColor;
+import hu.elte.projeszk.Card.CardValue;
+
+public class ClientMachine {
+
+	 public static void main(String[] args) {
+		 
+				
+		 
+		 
+
+}
+			
+public static CardValue convertCardValueFromCharacter(char c){
+	
+	CardValue cardValue=null;
+		
+	//switch elágazással
+	return cardValue;
+}	
+
+public static CardColor convertCardColorFromCharacter(char c){
+	
+	CardColor cardColor=null;
+	
+	//switch elágazással
+	return cardColor;
+}
+
+public Card machineCardChooseAlgorithm(ArrayList<Card> cardInHand, boolean lastPlayerDrawed, CardColor DeclaredColor  ){
+
+	//szabályok egy kártyát ad vissza az alapján hogy mi van a kezében, az előző játékos huzott-e és van e színkötelezettség
+	
+	
+return null;
+	
+}
+
+
+
+}

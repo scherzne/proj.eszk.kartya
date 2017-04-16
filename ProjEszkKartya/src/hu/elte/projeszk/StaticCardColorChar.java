ee5c7bd88ddee74dc3b4e7a9dd29520f67445253
HuszarGyula <gyuland@gmail.com>
2017-04-16 23:21:57 +0200

    Csináltam egy osztályt csak statikus methodokkal, kartakter-Szín(enum) átalakítást egységes és elérhető legyen a szerver és kliens osztályokban.

---
+++ ProjEszkKartya/src/hu/elte/projeszk/StaticCardColorChar.java
@@ -0,0 +1,42 @@
+package hu.elte.projeszk;
+
+import  hu.elte.projeszk.Card.CardColor;
+
+public class StaticCardColorChar {
+	
+	public static CardColor convertCharacterToCardColor(char c){
+		
+		CardColor cardColor=null;
+		
+		//switch elágazással
+		
+		
+	 switch(c){
+	 
+	 case('P'): cardColor= CardColor.PIROS;break;
+	 case('K'): cardColor= CardColor.KEK;break;
+	 case('Z'): cardColor= CardColor.ZOLD;break;
+	 case('S'): cardColor= CardColor.SARGA;break;
+	 case('F'): cardColor= CardColor.FEKETE;break;
+	 }
+		return cardColor;
+	}
+	
+	
+public static char convertCardColorToCharacter(CardColor cardColor){
+		
+		char c=' ';
+		
+		 switch(cardColor){
+		 
+		 case PIROS:  c='P';break;
+		 case KEK:  c='K'; break;
+		 case ZOLD: c='Z';break;
+		 case SARGA:  c='S';break;
+		 case FEKETE:  c='F';break;
+		 }
+		//switch elágazással
+		return c;
+	}
+
+}

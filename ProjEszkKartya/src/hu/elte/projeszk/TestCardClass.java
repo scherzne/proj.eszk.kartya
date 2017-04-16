55108c88fa20b1bddc937146c1d1e7e9c07d4199
HuszarGyula <gyuland@gmail.com>
2017-04-16 23:23:12 +0200

    A tesztet kibővítettem példákkal a szín(enum)- karakter átalakításokhoz.

--- ProjEszkKartya/src/hu/elte/projeszk/TestCardClass.java
+++ ProjEszkKartya/src/hu/elte/projeszk/TestCardClass.java
@@ -7,11 +7,11 @@ import org.junit.Test;
 import  hu.elte.projeszk.Card;
 import  hu.elte.projeszk.Card.CardColor;
 import  hu.elte.projeszk.Card.CardValue;
-
+import  hu.elte.projeszk.StaticCardColorChar;
 
 public class TestCardClass {
 	
-	 final CardColor cardColor = CardColor.K;
+	 final CardColor cardColor = CardColor.KEK;
 	 final CardValue cardValue = CardValue.HAROM;
 	 
 	  @Test
@@ -23,9 +23,20 @@ public class TestCardClass {
 	       // String result = myUnit.concatenate("one", "two");
 	       // assertEquals("onetwo", result);
 		  
-		  assertEquals(CardColor.K ,myCard.getCardColor());
+		  assertEquals(CardColor.KEK,myCard.getCardColor());
 		  assertEquals(CardValue.HAROM ,myCard.getCardValue());
 		  
 		//  assertEquals(CardValue.NEGY ,myCard.getCardValue());
+		   assertEquals(StaticCardColorChar.convertCharacterToCardColor('P'),CardColor.PIROS);
+		   assertEquals(StaticCardColorChar.convertCharacterToCardColor('K'),CardColor.KEK);
+			 
+		   assertEquals(StaticCardColorChar.convertCardColorToCharacter(CardColor.FEKETE),'F');
+		   assertEquals(StaticCardColorChar.convertCardColorToCharacter(CardColor.ZOLD),'Z');
+			
+		   
+		   Card myCard2 = new Card(StaticCardColorChar.convertCharacterToCardColor('S'),cardValue);
+		   assertEquals(myCard2.getCardColor(),CardColor.SARGA);
+			
+		   
 	  }
 }

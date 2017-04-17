97ae5adf723fa919d2e34c7c56939340236bac94
scherzne <scherzne@gmail.com>
2017-04-17 10:30:51 +0200

    Player read-write, getter-ek

--- ProjEszkKartya/src/hu/elte/projeszk/server/Player.java
+++ ProjEszkKartya/src/hu/elte/projeszk/server/Player.java
@@ -28,5 +28,77 @@ public class Player {
 		this.id=id;
 		this.live=true;
 	}
+	/**
+	 * Csak olvas egyet a scanner-el
+	 * @return
+	 */
+	public String read(){
+		String w=null;
+
+		try{
+			if(scanner.hasNextLine()){				
+				w=scanner.nextLine();
+				updateLastAction();
+			}else die();
+		}catch (IllegalStateException e) {
+			die();
+			w=null;
+		}
+		
+		return w;
+	}
+	/**
+	 * Ír a saját kimenetére
+	 * @param str
+	 */
+	public void write(String str){
+		pw.println(str);
+		pw.flush();
+	}
+
+	public Socket getSocket() {
+		return socket;
+	}
+
+	public void setSocket(Socket socket) {
+		this.socket = socket;
+	}
+
+	public long getLastAction() {
+		return lastAction;
+	}
+
+	public synchronized void updateLastAction() {
+		this.lastAction = System.currentTimeMillis();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public synchronized boolean isLive() {
+		return live;
+	}
+
+	/**
+	 * Nem élővé tesszük a játékost
+	 */
+	public void die() {
+		this.live = false;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return name;
+	}
+
 
 }

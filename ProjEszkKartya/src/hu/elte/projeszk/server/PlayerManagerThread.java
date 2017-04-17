5287b950d9b24a89a01b6258f2ddfe41c5ae913b
scherzne <scherzne@gmail.com>
2017-04-17 10:14:08 +0200

    kártyapakli generálás megvalósítása

--- ProjEszkKartya/src/hu/elte/projeszk/server/PlayerManagerThread.java
+++ ProjEszkKartya/src/hu/elte/projeszk/server/PlayerManagerThread.java
@@ -5,14 +5,27 @@ import hu.elte.projeszk.Card.CardColor;
 import hu.elte.projeszk.Card.CardValue;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 
 public class PlayerManagerThread extends Thread {
 	private ArrayList<Card> cardPack=new ArrayList<>();
+	private HashMap<Integer, PlayerThread> players;
 	private int id=-1;	
 	
-	public PlayerManagerThread(int id) {
+	private int nextPlayer=0;
+	private boolean isRunning=false; 
+	
+	/**
+	 * A szerver begyűjtötte a játszani óhajtott csoportot, a manager innen átveheti az irányítást
+	 * A szerver várhat a köv. adag játékosra
+	 * @param id
+	 * @param players
+	 */
+	public PlayerManagerThread(int id,HashMap<Integer, PlayerThread> players) {
 		super("manager "+id);
 		this.id=id;
+		this.players=players;
 		
 		generatePack();
 	}
@@ -23,12 +36,37 @@ public class PlayerManagerThread extends Thread {
 	private void generatePack(){
 		Card card;
 		
+		//összes szín
 		for(int c=CardColor.SARGA.ordinal();c<CardColor.FEKETE.ordinal();c++){
+			//egy-egy darab 0-ás
 			card=new Card(c, Card.cardValueToInt(CardValue.NULLA));
 			cardPack.add(card);
-			for(int i=CardValue.EGY.ordinal();i<CardValue.FORDITTO.ordinal();i++){
-				card=new Card(c, i);
+			//két darab többi számos, plusz húzz kettőt, fordulj, ugorj
+			for(int j=0;j<2;j++){
+				//számos
+				for(int i=CardValue.EGY.ordinal();i<CardValue.FORDITTO.ordinal();i++){
+					card=new Card(c, i);
+					cardPack.add(card);
+				}
+				//húzz kettőt
+				card=new Card(c, Card.cardValueToInt(CardValue.HUZZKETTOT));
+				cardPack.add(card);
+				//fordulj
+				card=new Card(c, Card.cardValueToInt(CardValue.FORDITTO));
+				cardPack.add(card);
+				//ugorj
+				card=new Card(c, Card.cardValueToInt(CardValue.KIMARADSZ));
+				cardPack.add(card);
 			}
 		}
+		//fekete joker és húzz négyet joker
+		for(int i=0;i<4;i++){
+			card=new Card(CardColor.FEKETE, CardValue.SZINKEREO);
+			cardPack.add(card);
+			card=new Card(CardColor.FEKETE, CardValue.HUZZNEGYET);
+			cardPack.add(card);
+		}
+		//pakli megkeverése
+		Collections.shuffle(cardPack);
 	}
 }

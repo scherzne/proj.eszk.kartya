0d2f69b97653642507e3e1e73896c962dbec9cf0
scherzne <scherzne@gmail.com>
2017-04-17 10:40:39 +0200

    Módosítás: ebbe lesz a szerver main

--- ProjEszkKartya/src/hu/elte/projeszk/server/CardServer.java
+++ ProjEszkKartya/src/hu/elte/projeszk/server/CardServer.java
@@ -1,20 +1,22 @@
 package hu.elte.projeszk.server;
 
+import hu.elte.projeszk.Consts;
+
+import java.io.IOException;
 import java.net.ServerSocket;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class CardServer {
-	private HashMap<Integer, PlayerThread> players;
-	private static int counter=0;
-	
-	private ServerSocket serverSocket=null;
-	
-	public CardServer() {
-		super();
-		players=new HashMap<>();
-	}
+	private static ServerSocket server=null;	
+
+	public static void main(String[] args) throws IOException {
+		server=new ServerSocket(Consts.PORT);
 		
-	public boolean startServer(){
-		return false;
+		//játékos gyűjtő
+		ArrayList<Player> players=new ArrayList<>();
+		//managereket is begyűjtjük, hogy ne lógjanak a levegőben
+		ArrayList<PlayerManagerThread> managers=new ArrayList<>();
+
 	}
 }

6bc6d1068a5b8de61ca9ce8c43daf794975923f1
scherzne <scherzne@gmail.com>
2017-04-17 16:48:41 +0200

    refaktor!

--- ProjEszkKartya/src/hu/elte/projeszk/Card.java
+++ ProjEszkKartya/src/hu/elte/projeszk/Card.java
@@ -159,6 +159,70 @@ public class Card {
 		return c;
 	}
 	
+	/**
+	 * Áthelyezve StaticCardValueChar-ból. Logikailag ide tartozik
+	 * Továbbra is: nem értek egyet a beégetett kódokkal!
+	 * @param c
+	 * @return
+	 */
+	public static CardValue convertCharacterToCardValue(char c){		
+		CardValue cardValue=null;
+		
+		//switch elágazással		
+		 switch(c){		 
+			 case('0'): cardValue= CardValue.NULLA;break;
+			 case('1'): cardValue= CardValue.EGY;break;
+			 case('2'): cardValue= CardValue.KETTO;break;
+			 case('3'): cardValue= CardValue.HAROM;break;
+			 case('4'): cardValue= CardValue.NEGY;break;
+			 case('5'): cardValue= CardValue.OT;break;
+			 case('6'): cardValue= CardValue.HAT;break;
+			 case('7'): cardValue= CardValue.HET;break;
+			 case('8'): cardValue= CardValue.NYOLC;break;
+			 case('9'): cardValue= CardValue.KILENC;break;
+			
+			 case('H'): cardValue= CardValue.HUZZKETTOT;break;
+			 case('K'): cardValue= CardValue.KIMARADSZ;break;
+			 case('M'): cardValue= CardValue.FORDITTO;break;
+			 
+			 case('S'): cardValue= CardValue.SZINKEREO;break;
+			 case('N'): cardValue= CardValue.HUZZNEGYET;break;
+		 }
+		return cardValue;
+	}
+	
+	/**
+	 * Áthelyezve StaticCardValueChar-ból. Logikailag ide tartozik
+	 * Továbbra is: nem értek egyet a beégetett kódokkal!
+	 * @param cardValue
+	 * @return
+	 */
+	public static char convertCardValueToCharacter(CardValue cardValue){		
+		char c=' ';
+		
+		 switch(cardValue){		 
+			 case NULLA:  c='0';break;
+			 case EGY:    c='1'; break;
+			 case KETTO:  c='2';break;
+			 case HAROM:  c='3';break;
+			 case NEGY:   c='4';break;
+			 case OT:     c='5';break;
+			 case HAT:    c='6';break;
+			 case HET:    c='7';break;
+			 case NYOLC:  c='8';break;
+			 case KILENC: c='9';break;
+			 
+			 case HUZZKETTOT:  c='H';break;
+			 case FORDITTO:    c='M';break;
+			 case KIMARADSZ:   c='K';break;
+			 
+			 case SZINKEREO:   c='S';break;
+			 case HUZZNEGYET:  c='N';break;		 
+		 }
+		//switch elágazással
+		return c;
+	}
+
 	public CardColor getCardColor() {
 		return cardColor;
 	}

c3962b81740589d78869b22baa6b80e06e1521fd
HaDani <hadani@citromail.hu>
2017-04-23 15:20:42 +0200

    Add files via upload

---
+++ ProjEszkKartya/src/hu/elte/projeszk/client/ManualClient.java
@@ -0,0 +1,105 @@
+package hu.elte.projekteszk.client;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+
+import hu.elte.projekteszk.Card;
+import hu.elte.projekteszk.Card.CardColor;
+import hu.elte.projekteszk.Card.CardValue;
+import hu.elte.projekteszk.Consts;
+
+public class ManualClient {
+	private BufferedReader br;
+	private PrintWriter pw;
+	private Scanner sc;
+	private String clientName;
+	private ArrayList<Card> cardsInHand;
+	private boolean hasEnded;
+
+	Socket socket = null;
+
+	public static void main(String[] args) throws UnknownHostException, IOException {
+		ManualClient client = new ManualClient();
+
+		client.communicate();
+	}
+
+	public ManualClient() throws UnknownHostException, IOException {
+		cardsInHand = new ArrayList<Card>();
+		socket = new Socket("localhost", Consts.PORT);
+		pw = new PrintWriter(socket.getOutputStream(), true);
+
+		br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		sc = new Scanner(socket.getInputStream());
+
+		hasEnded = false;
+
+	}
+
+	public void communicate() throws IOException {
+
+		String serverMessageString = "";
+		BufferedReader stdinReader = new BufferedReader(new InputStreamReader(System.in));
+
+		while (!serverMessageString.equals("ok")) {
+			System.out.print("Nev: ");
+
+			clientName = stdinReader.readLine();
+			pw.println(clientName);
+			serverMessageString = br.readLine();
+		}
+
+		System.out.println("Sikeres csatlakozas.");
+
+		try {
+			do {
+				String stringToWriteToMonitor = clientName + ": ";
+
+				serverMessageString = br.readLine();
+
+				if (serverMessageString.charAt(0) == 'A') {
+					int numberOfSendedCards = Character.getNumericValue(serverMessageString.charAt(1));
+					for (int i = 0; i < numberOfSendedCards; i++) {
+						serverMessageString = br.readLine();
+					}
+				}
+
+				if (serverMessageString.charAt(0) == 'L') {
+
+					List<String> messages = new ArrayList<String>();
+					messages = Arrays.asList(serverMessageString.split(","));
+
+					CardColor cColor = Card.convertCharacterToCardColor(messages.get(1).charAt(0));
+					CardValue cValue = Card.convertCharacterToCardValue(messages.get(1).charAt(1));
+
+					Card card = new Card(cColor, cValue);
+					cardsInHand.add(card);
+
+				}
+
+				if (serverMessageString.charAt(0) == 'S') {
+
+					
+				}
+
+			} while (!hasEnded);
+
+			System.out.println(clientName + " HASENDED!");
+
+		} catch (IOException e) {
+			System.err.println("Kommunikacios hiba a fogado szalban.");
+		}
+
+	}
+}
